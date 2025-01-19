d5f3d7c8c146fecaedc312e89931398e88b09ed1
Dolapo <dolapo.adeeyo1@gmail.com>
2025-01-19 17:34:00 +0000

    Updated terraform apply script

--- CICD/apply-terraform.sh
+++ CICD/apply-terraform.sh
@@ -6,11 +6,17 @@ set -eu
 # go back to the previous directory
 cd ..
 
+#creating a temporary .tfvar file
+echo "Fetching parameters from Parameter Store"
+aws ssm get-parameters-by-path --path "/terraform/dev" --with-decryption --query "Parameters[].{Name:Name,Value:Value}" --output json > parameters.json
+echo "Building dev.tfvars file"
+jq -r '.[] | "\(.Name | split("/")[-1]) = \"\(.Value)\""' parameters.json > dev.tfvars
+
 # initialize terraform
-terraform init
+terraform init -backend-config="env/backend_s3_dev.hcl"
 
 # apply terraform
-# terraform apply -auto-approve
+terraform apply -var-file="dev.tfvars" -auto-approve
 
 # destroy terraform
-terraform destroy -auto-approve
+# terraform destroy -auto-approve
